43833e6e7a693fbcebd671e565db0e7c13de5dd2
Nick Arocho <nick@boldlyfine.com>
2017-12-19 23:26:58 -0800

    Create dirs, create board array, create timer, create bomb bount, style board

---
+++ js/cell.js
@@ -0,0 +1,39 @@
+class  Cell {
+    constructor(row, col, board) {
+        this.row = row;
+        this.col = col;
+        this.bomb = (Math.random() < .155);
+        this.board = board;
+        this.revealed = false;
+    }
+
+    getAdjCells() {
+        // return an array of adj cells
+        var adj = [];
+        var lastRow = board.length - 1;
+        var lastCol = board[0].length - 1;
+
+        if (this.row > 0 && this.col > 0) adj.push(board[this.row - 1][this.col - 1]);
+        if (this.row > 0) adj.push(board[this.row - 1][this.col]);
+        if (this.row > 0 && this.col < lastCol) adj.push(board[this.row - 1][this.col + 1]);
+        if (this.col < lastCol) adj.push(board[this.row][this.col + 1]);
+        if (this.row < lastRow && this.col < lastCol) adj.push(board[this.row + 1][this.col + 1]);
+        if (this.row < lastRow) adj.push(board[this.row + 1][this.col]);
+        if (this.row < lastRow && this.col > 0) adj.push(board[this.row + 1][this.col - 1]);
+        if (this.col > 0) adj.push(board[this.row][this.col - 1]);       
+        return adj;
+    }
+
+    calcAdjBombs() {
+        var adjCells = this.getAdjCells();
+        var adjBombs = adjCells.reduce(function(acc, cell) {
+            return acc + (cell.bomb ? 1 : 0);
+        }, 0);
+        this.adjBombs = adjBombs;
+    }
+
+    reveal() {
+        this.revealed = true;
+        // if this.adjBombs === 0 then call reveal on all adj cells if that cell is not a bomb
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,114 @@
+/*----- constants -----*/
+var bombImage = `<img src="./../images/bomb.png">`;
+
+/*----- app's state (variables) -----*/
+var size;
+var board;
+var timer;
+var bombCount;
+var timeElapsed;
+var adjBombs;
+
+/*----- cached element references -----*/
+var boardEl = document.getElementById('board');
+
+/*----- event listeners -----*/
+document.getElementById('size-btns').addEventListener('click', function(e) {
+  size = parseInt(e.target.id.replace('size-', ''));
+  init();
+  render();
+});
+
+boardEl.addEventListener('click', function(e) {
+  // setTimer();
+  if (e.target.classList.contains('game-cell')) {
+    console.log('clicked')
+    // var rowIdx = parseInt(td.getAttribute('data-row'));
+    // var colIdx = parseInt(td.getAttribute('data-col'));
+  }
+  render();
+});
+
+/*----- functions -----*/
+function setTimer () {
+  setInterval(function(){
+    timeElapsed += 1;
+    render();
+  }, 1000)
+};
+
+function buildTable() {
+  var topRow = `
+    <tr>
+        <td class="menu" colspan="${size}">
+            <section id="status-bar">
+                <div id="bomb-counter">000</div>
+                <div id="reset"><img src="images/smiley-face.png"></div>
+                <div id="timer">000</div>
+            </section>
+        </td>
+    </tr>
+    `;
+  boardEl.innerHTML = topRow + `<tr>${'<td class="game-cell"></td>'.repeat(size)}</tr>`.repeat(size);
+  var cells = Array.from(document.querySelectorAll('td:not(.menu)'));
+  cells.forEach(function(cell, idx) {
+    cell.setAttribute('data-row', Math.floor(idx / size));
+    cell.setAttribute('data-col', idx % size);
+  });
+}
+
+function buildArrays() {
+  var arr = Array(size).fill(null);
+  arr = arr.map(function() {
+    return new Array(size).fill(null);
+  });
+  return arr;
+};
+
+function buildCells(){
+  board.forEach(function(rowArr, rowIdx) {
+    rowArr.forEach(function(slot, colIdx) {
+      board[rowIdx][colIdx] = new Cell(rowIdx, colIdx, board);
+    });
+  });
+  board.forEach(function(rowArr) {
+    rowArr.forEach(function(cell) {
+      cell.calcAdjBombs();
+    });
+  });
+};
+
+function init() {
+  buildTable();
+  board = buildArrays();
+  buildCells();
+  timeElapsed = 0;
+  bombCount = getBombCount();
+};
+
+function getBombCount() {
+  bombCount = 0;
+  board.forEach(function(row){
+    bombCount += row.filter(function(cell) {
+      return cell.bomb;
+    }).length
+  });
+  return bombCount.toString().padStart(3, '0');
+};
+
+function render() {
+  document.getElementById('bomb-counter').innerText = bombCount;
+  var seconds = timeElapsed % 60;
+  document.getElementById('timer').innerText = seconds.toString().padStart(3, '0');
+  var tdList = Array.from(document.querySelectorAll('[data-row]'));
+  tdList.forEach(function(td) {
+    var rowIdx = parseInt(td.getAttribute('data-row'));
+    var colIdx = parseInt(td.getAttribute('data-col'));
+    if (board[rowIdx][colIdx].revealed) {
+      td.textContent = board[rowIdx][colIdx].bomb ? 'B' : board[rowIdx][colIdx].adjBombs;
+    }
+  });
+};
+
+init();
+render();
